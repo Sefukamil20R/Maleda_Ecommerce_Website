90f0195b843336094e7c70152ad2bdd6b621adae
solomon Abate <solomonabate18@gmail.com>
2024-05-24 08:46:03 +0300

    session bug fix

--- admin/session_check.php
+++ admin/session_check.php
@@ -2,7 +2,9 @@
 if (count(get_included_files()) == 1) exit("Direct access not permitted.");
 
 // Start the session
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Check if the user is logged in
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
